4b76696cd6d33dce0ee67ea92d28934ab073ef13
erikaprussin <erika.russin@escola.pr.gov.br>
2025-06-18 11:57:35 -0300

    Add files via upload

---
+++ projeto card da Erika/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Qual as civilizações mais importantes da história?',
+    'Antigo Egito, Gŕecia antiga e Roma antiga.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Honduras?',
+    'A capital de Honduras é Tegucigalpa'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Malásia?',
+    'A capital da Malásia é o Território Federal de Kuala Lumpur.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amarelo em inglês?',
+    'Amarelo em inglês é Yellow'
+)
